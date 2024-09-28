a8fa6511bdeb8419537439603c043cc30efa78a9
Ricardo  Antonio Cruz Cruz <ext.ricardo.cruz@coloit.com>
2024-09-28 03:50:33 -0500

    Updated router.options

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,17 +1,25 @@
 import type { RouterConfig } from "@nuxt/schema";
-// https://router.vuejs.org/api/interfaces/routeroptions.html
-export default <RouterConfig>{
-    scrollBehavior: (to, from, savedPosition) => {
-        if (savedPosition) {
-            return savedPosition;
-        }
 
-        // if both from and to paths contain /galleries then scroll to top
-        if (from.path.includes('/galleries') && to.path.includes('/galleries')) {
-            return { top: 0 }
-        }
-        
-        return { top: 0 }
-    },
+export default <RouterConfig>{
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    }
     
+    // if both from and to paths contain /galleries then scroll to top
+    if (from.path.includes('/galleries') && to.path.includes('/galleries')) {
+      return { top: 0 };
+    }
+    
+    return { top: 0 };
+  },
+  routes: (_routes) => {
+    const baseRoutes = _routes.map(route => {
+      return {
+        ...route,
+        path: `/camaranja${route.path}`
+      }
+    });
+    return [...baseRoutes, ..._routes];
+  }
 };
