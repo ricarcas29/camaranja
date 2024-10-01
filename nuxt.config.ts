5f3f430f04ad436b11d06b0ca9ced3e51d6bcb3a
Ricardo  Antonio Cruz Cruz <ext.ricardo.cruz@coloit.com>
2024-09-30 23:30:45 -0500

    Updated Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,8 @@ dotenv.config();
 export default defineNuxtConfig({
   pages: true,
   ssr: true,
-  router : {
-    base: process.env.NUXT_APP_BASE_URL || "/camaranja/",
+  router: {
+    base: '/'
   },
   target: "static",
   experimental: {
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     },
   },
   routeRules: {
-    "/**": { ssr: true }, // Habilita el SSR para todas las rutas
+    "/**": { ssr: true },
   },
   build: {
     extractCSS: true,
@@ -33,29 +33,12 @@ export default defineNuxtConfig({
         },
       },
     },
-    analyze: true, // Ayuda a identificar paquetes grandes
-    extractCSS: true,
+    analyze: true,
     optimization: {
       splitChunks: {
         layouts: true,
         pages: true,
         commons: true,
-        chunks: "all",
-        automaticNameDelimiter: ".",
-        name: undefined,
-        cacheGroups: {},
-      },
-    },
-    babel: {
-      presets({ isServer }) {
-        return [
-          [
-            "@nuxt/babel-preset-app",
-            {
-              corejs: { version: 3 },
-            },
-          ],
-        ];
       },
     },
     transpile: ["vue-lazy-hydration", "intersection-observer"],
@@ -76,7 +59,6 @@ export default defineNuxtConfig({
   render: {
     http2: {
       push: true,
-      pushAssets: (req, res, publicPath, preloadFiles) => preloadFiles.filter((f) => f.asType === "script" && f.file === "runtime.js").map((f) => `<${publicPath}${f.file}>; rel=preload; as=${f.asType}`),
     },
     compressor: {
       threshold: 0,
@@ -87,13 +69,20 @@ export default defineNuxtConfig({
   htmlAttrs: {
     lang: "es",
   },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge", "nuxt-headlessui", "@nuxt/content", "@vueuse/nuxt", "@nuxtjs/color-mode", "@nuxt/icon", "@nuxtjs/seo"],
-  // Configuración de imágenes
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image-edge",
+    "nuxt-headlessui",
+    "@nuxt/content",
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxt/icon",
+    "@nuxtjs/seo",
+  ],
   image: {
-
     inject: true,
     format: ["webp", "avif", "jpg"],
-    domains: ["localhost", "camaranja.nuxt.space.com","camaranja-portfolio.vercel.app/"],
+    domains: ["localhost"],
     dir: "public",
     presets: {
       default: {
@@ -102,7 +91,6 @@ export default defineNuxtConfig({
         },
       },
     },
-
     screens: {
       xs: 320,
       sm: 640,
@@ -114,83 +102,55 @@ export default defineNuxtConfig({
     },
     densities: [1, 2],
   },
-  // Configuración de la aplicación
   app: {
     pageTransition: { name: "page", mode: "out-in" },
-    baseURL: process.env.NUXT_APP_BASE_URL || "/camaranja/",
+    baseURL: '/',
     buildAssetsDir: "/_nuxt/",
     head: {
       link: [
-        {
-          rel: "preconnect",
-          href: "https://fonts.googleapis.com",
-        },
-        {
-          rel: "preconnect",
-          href: "https://fonts.gstatic.com",
-        },
-        {
-          href: "https://fonts.googleapis.com/css2?Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,400;0,600;0,700;1,400&display=swap",
-          rel: "stylesheet",
-        },
-        {
-          rel: "preload",
-          as: "font",
-          href: "https://fonts.googleapis.com/css2?Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,400;0,600;0,700;1,400&display=swap",
-          crossorigin: "anonymous",
-        },
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,400;0,600;0,700;1,400&display=swap" },
       ],
     },
   },
-  // Configuración del contenido
   content: {
     documentDriven: true,
     markdown: {
       anchorLinks: false,
     },
   },
-  // Configuración del modo oscuro
   colorMode: {
     classSuffix: "",
   },
-  // Configuración de Tailwind CSS
   tailwindcss: {
     cssPath: "./assets/css/tailwind.css",
   },
-  // Configuración de iconos
   icon: {
     size: [16, 24, 32, 120],
   },
-  // Información del sitio
   site: {
     url: process.env.BASE_URL,
     name: "Kenia Espinoza Alba Portfolio",
     description: "Conóceme, contáctame, ¡creemos recuerdos juntos!",
     defaultLocale: "es",
   },
-
-  // Configuración de Nitro para la generación estática
   nitro: {
     compressPublicAssets: true,
     prerender: {
       crawlLinks: true,
-      routes: ["/camaranja", "/galleries", "/stories", "/hire-me"],
+      routes: ["/", "/galleries", "/stories", "/hire-me"],
       failOnError: false,
     },
   },
-
-  // Configuración de Schema.org
   schemaOrg: {
     identity: {
       type: "Person",
       name: "Kenia Espinoza Alba",
-      url: process.env.NUXT_APP_BASE_URL ?? "/camaranja/",
+      url: "/",
       logo: "img/logo.jpg",
     },
   },
-
   ...(process.env.NUXT_STUDIO_ENABLE ? { extends: "@nuxthq/studio" } : {}),
-
-  // Fecha de compatibilidad
   compatibilityDate: "2024-09-23",
 });
